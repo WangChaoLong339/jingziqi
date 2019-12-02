445742a692941f57550862be665106005cd7ca46
WangChaoLong339 <1628951883@qq.com>
2019-12-02 15:40:33 +0800

    添加 分组发送

--- service/start.js
+++ service/start.js
@@ -6,7 +6,7 @@ const LoginManager = require('./LoginManager.js')
 const HallManager = require('./HallManager.js')
 const GameManager = require('./GameManager.js')
 
-console.log('-------------服务器启动成功-------------')
+console.log('-------------服务器---启动成功-------------')
 io.on('connection', (socket) => {
     // 断开
     socket.on('disconnect', (err) => {
@@ -15,9 +15,13 @@ io.on('connection', (socket) => {
     // 登陆
     socket.on('cts_login', (data) => {
         LoginManager.login(data, (msg) => {
-            socket.join('group_hall')
             socket.emit('stc_login', msg)
         })
+    })
+
+    // 加入大厅
+    socket.on('group_hall', (data) => {
+        socket.join('group_hall')
     })
 
     // 同步大厅
@@ -31,17 +35,16 @@ io.on('connection', (socket) => {
     socket.on('cts_create_game', (data) => {
         GameManager.createGame(data, (msg) => {
             io.sockets.in('group_hall').emit('stc_create_game', msg)
-            socket.emit('stc_create_game', msg)
         })
     })
 
     // 进入房间
     socket.on('cts_enter_game', (data) => {
-        GameManager.enterGame(data, function (msg) {
+        GameManager.enterGame(data, (msg) => {
             io.sockets.in(data.gameId).emit('stc_enter_game', msg)
             socket.emit('stc_enter_game', msg)
             socket.join(data.gameId)
-        }.bind(this))
+        })
     })
 
     // 同步房间
